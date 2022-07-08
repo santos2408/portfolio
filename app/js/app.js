74ba5590869b776e1da6ff7cd01658b5002f0038
Roger Santos <roger.santos36@gmail.com>
2022-07-08 15:44:11 -0300

    :recycle: refactor: refatorado cards da lista para div's e corrigido bug nas imagens
    as imagens dos projetos estavam apresentando uma linha branca no topo delas

--- app/js/app.js
+++ app/js/app.js
@@ -9,3 +9,31 @@ menuOpen.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
   menu.classList.remove('show-menu')
 })
+
+// CHANGE THEME OF WEBSITE
+
+const themeButton = document.querySelector('[data-js="theme-button"]')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => 
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+const getCurrentIcon = () => 
+  document.body.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-sun' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
